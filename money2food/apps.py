962da3d0ea19786e776c27453b65e9cf8ada9c66
timos222 <timo.savva.ebay@gmail.com>
2020-07-18 10:33:09 +0300

    fixed untracked files

--- money2food/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Money2FoodConfig(AppConfig):
-    name = 'money2food'
--- money2food/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from money2food.views import *
-
-app_name = 'money2food'
-urlpatterns = [
-    # path('', first_page, name='fp'),
-]
