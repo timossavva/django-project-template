8cb2d6a72fc36a843c5133d050c0eaca1957fafa
timos222 <timos.savva.ebay@gmail.com>
2021-06-29 10:54:42 +0300

    fhm

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,3 +123,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
+
+# ----------------------------------------------- Django Specific Settings ------------------------------------------- #
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
